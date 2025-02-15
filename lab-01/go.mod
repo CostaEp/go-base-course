f2c3df785a0a0020fbf7d354a74e797fc2b2e901
Costa epshtein <costaepshtein@Costas-MBP-14.lan>
2025-02-15 18:25:39 +0200

    lab-01: learn first steps at go

---
+++ lab-01/go.mod
@@ -0,0 +1,3 @@
+module demo/first-app-01
+
+go 1.24.0
--- lab-01/main.go
+++ lab-01/main.go
@@ -1,7 +1,30 @@
+// package main is the entry point of the go program
 package main
 
+// import fmt package to use its functions
 import "fmt"
 
+// main function is the entry point of the go program
 func main() {
-	fmt.Printf("hello , its my first line code in go language")
+	fmt.Printf("hello , its my first line code in go language \n")
+	fmt.Printf("fmt.Print allow us to print something to the console")
 }
+
+// if we whant to build this app we need to initialize the go module by run this command:
+// go mod init <module_name>
+// -------------------------------------------
+// then we can build the app by run this command:
+// go build
+// -------------------------------------------
+// lets try this at the command line
+// -------------------------------------------
+//now we can run the app this command:
+// ./<module_name>
+// -------------------------------------------
+// output:
+// hello , its my first line code in go language
+// fmt.Print allow us to print something to the console
+// -------------------------------------------
+// if we want to run the app without build it we can run this command:
+// go run main.go
+// -------------------------------------------
